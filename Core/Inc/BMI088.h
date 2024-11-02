af2098b249d8e3ef94f53a82c2a2fe037ce1d2ad
dulinnaire <150122917+dulinnaire@users.noreply.github.com>
2024-11-02 13:55:58 +0800

    Initialization

---
+++ Core/Inc/BMI088.h
@@ -0,0 +1,8 @@
+//
+// Created by 81301 on 2024/11/2.
+//
+
+#ifndef BMI088_H
+#define BMI088_H
+
+#endif //BMI088_H
---
+++ Core/Src/callback.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 81301 on 2024/11/2.
+//
+
+#include "tim.h"
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim) {
+    if (htim == &htim1) {
+    }
+}
